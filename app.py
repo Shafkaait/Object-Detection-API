f58f69aef7cb0c79767e63133f2e994e71a4445f
Shafkaait <33679528+Shafkaait@users.noreply.github.com>
2021-04-22 23:24:16 +0600

    Update app.py

--- app.py
+++ app.py
@@ -134,4 +134,4 @@ def get_image():
     except FileNotFoundError:
         abort(404)
 if __name__ == '__main__':
-    app.run(debug=True, host = '0.0.0.0', port=5050)
+    app.run(debug=True, port=5050)
